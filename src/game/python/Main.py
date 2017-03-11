60c3fbdb08afde984490a2de45657bbf83b8b594
Per-Arne Andersen <perara12@gmail.com>
2017-03-12 00:06:53 +0100

    DQN work

--- src/game/python/Main.py
+++ src/game/python/Main.py
@@ -6,4 +6,24 @@ print("Running PyAI using Python %s" % (sys.version))
 PyAPIRegistry.loaded() # Tell C++ that we are done with dependencies
 
 pyai = PyAI(0, 0)  # Create new AI hook with gameID = 0 playerID= 0
-dqn = DQN(pyai)  # Create new DQN using the pyai hook
+dqn = DQN()  # Create new DQN using the pyai hook
+
+num_episodes = 20
+
+for e in xrange(num_episodes):
+    observation = pyai.getState()
+    done = False
+    agent.new_episode()
+    total_cost = 0.0
+    total_reward = 0.0
+    frame = 0
+    while not done:
+        frame += 1
+        #env.render()
+        action, values = dqn.act(observation)
+        #action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        total_cost += agent.observe(reward)
+        total_reward += reward
+    print("total reward", total_reward)
+    print("mean cost", total_cost/frame)
