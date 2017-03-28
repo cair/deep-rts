8ffd9199009ca43ddfe253098f92149d62b16ebd
Per-Arne Andersen <perara12@gmail.com>
2017-03-28 08:11:11 +0200

    Some updates for centos

--- c++/src/flatbuffers/ScoreLog/create_buffer.sh
+++ c++/src/flatbuffers/ScoreLog/create_buffer.sh
@@ -2,4 +2,4 @@
 
 DIR=$(dirname "$0")
 mkdir -p $DIR/../../game/serialization/
-flatc --cpp --python -o $DIR/../../game/serialization/ -b $DIR/ScoreItem.fbs $DIR/ScoreLog.fbs $DIR/ScorePlayer.fbs
+/usr/local/bin/flatc --cpp --python -o $DIR/../../game/serialization/ -b $DIR/ScoreItem.fbs $DIR/ScoreLog.fbs $DIR/ScorePlayer.fbs
